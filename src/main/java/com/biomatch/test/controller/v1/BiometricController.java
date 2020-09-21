62c3b2820218a7974e8993ff84db9ab9151ffaa8
sreelatha <sreelahta.kandagatla@unisys.com>
2020-09-21 13:51:51 -0400

    controller updated

--- src/main/java/com/biomatch/test/controller/v1/BiometricController.java
+++ src/main/java/com/biomatch/test/controller/v1/BiometricController.java
@@ -10,19 +10,19 @@ import java.util.ArrayList;
 import java.util.Base64;
 import java.util.List;
 
-import javax.validation.Valid;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.rekognition.AmazonRekognition;
 import com.amazonaws.services.rekognition.AmazonRekognitionClientBuilder;
+import com.amazonaws.services.rekognition.model.CompareFacesMatch;
+import com.amazonaws.services.rekognition.model.CompareFacesRequest;
+import com.amazonaws.services.rekognition.model.CompareFacesResult;
+import com.amazonaws.services.rekognition.model.ComparedFace;
+import com.amazonaws.services.rekognition.model.ComparedSourceImageFace;
 import com.amazonaws.services.rekognition.model.CreateCollectionRequest;
 import com.amazonaws.services.rekognition.model.CreateCollectionResult;
 import com.amazonaws.services.rekognition.model.DeleteCollectionRequest;
@@ -158,7 +158,7 @@ public class BiometricController {
 		//createCollection(collectionId);
 
 		//addFacesToCollection(collectionId,bucketName);
-		List<FaceMatchResult> faceMatchResultList = searchFaces(collectionId,bucketName);
+		List<FaceMatchResult> faceMatchResultList = compareFaces(bucketName);
 		ObjectMapper objectMapper = new ObjectMapper();
 		String result =  null;
 		
@@ -172,232 +172,51 @@ public class BiometricController {
 
 		return result;
 	}
-	/*
-	 * @GetMapping(value = "/")
-	 * 
-	 * @CrossOrigin(origins = "*") public void redirect(HttpServletResponse
-	 * response) throws IOException { response.sendRedirect("/swagger-ui.html"); }
-	 */
-
-	void createCollection(String collectionId) {
-		AmazonRekognition rekognitionClient = AmazonRekognitionClientBuilder.defaultClient();
-
-
-
-		System.out.println("Creating collection: " +
-				collectionId );
-
-		CreateCollectionRequest request = new CreateCollectionRequest()
-				.withCollectionId(collectionId);
-
-		CreateCollectionResult createCollectionResult = rekognitionClient.createCollection(request); 
-		System.out.println("CollectionArn : " +
-				createCollectionResult.getCollectionArn());
-		System.out.println("Status code : " +
-				createCollectionResult.getStatusCode().toString());
-	}
-
-	void addFacesToCollection(String collectionId, String bucketName) {
-		AmazonRekognition rekognitionClient = AmazonRekognitionClientBuilder.defaultClient();
+	
+	public List<FaceMatchResult> compareFaces(String bucketName) {	
+		
 		final AmazonS3 s3 = AmazonS3ClientBuilder.standard().withRegion(Regions.US_EAST_1).build();
 		ListObjectsV2Result result = s3.listObjectsV2(bucketName);
 		List<S3ObjectSummary> s3objectsummaryList = result.getObjectSummaries();
-		s3objectsummaryList.forEach(x -> indexFaces(collectionId, bucketName, x.getKey()));
-	}
-
-	void indexFaces(String collectionId, String bucketName, String object) {
-
-
-		AmazonRekognition rekognitionClient = AmazonRekognitionClientBuilder.defaultClient();
-
-		Image image = new Image()
-				.withS3Object(new S3Object()
-						.withBucket(bucketName)
-						.withName(object));
-
-
-		IndexFacesRequest indexFacesRequest = new IndexFacesRequest()
-				.withImage(image)
-				.withQualityFilter(QualityFilter.AUTO)
-				.withMaxFaces(1)
-				.withCollectionId(collectionId)
-				.withExternalImageId(object)
-				.withDetectionAttributes("DEFAULT");
-
-		IndexFacesResult indexFacesResult = rekognitionClient.indexFaces(indexFacesRequest);
-
-
-		/*System.out.println("Results for " + object);
-		System.out.println("Faces indexed:"); 
-		List<FaceRecord> faceRecords =		  indexFacesResult.getFaceRecords(); 
-		for (FaceRecord faceRecord : faceRecords)
-		{ System.out.println("  Face ID: " + faceRecord.getFace().getFaceId());
-		System.out.println("  Location:" +
-				faceRecord.getFaceDetail().getBoundingBox().toString()); }
-				*/
-
-
-
-	}
-
-	void cleanExistingCollection(String collectionId) {
-		boolean exists = false;
-		AmazonRekognition amazonRekognition = AmazonRekognitionClientBuilder.defaultClient();
-
-
-		//System.out.println("Listing collections");
-		int limit = 10;
-		ListCollectionsResult listCollectionsResult = null;
-		String paginationToken = null;
-		do {
-			if (listCollectionsResult != null) {
-				paginationToken = listCollectionsResult.getNextToken();
-			}
-			ListCollectionsRequest listCollectionsRequest = new ListCollectionsRequest()
-					.withMaxResults(limit)
-					.withNextToken(paginationToken);
-			listCollectionsResult=amazonRekognition.listCollections(listCollectionsRequest);
-
-			List < String > collectionIds = listCollectionsResult.getCollectionIds();
-			for (String resultId: collectionIds) {
-				//System.out.println(resultId);
-				if(resultId.equalsIgnoreCase(collectionId)) {
-					exists = true;
-					break;
-				}
-
-			}
-		} while (listCollectionsResult != null && listCollectionsResult.getNextToken() !=
-				null);
-		if(exists) {
-			DeleteCollectionRequest request = new DeleteCollectionRequest()
-					.withCollectionId(collectionId);
-			DeleteCollectionResult deleteCollectionResult = amazonRekognition.deleteCollection(request);     
-		}
-	}
-
-	List<FaceMatchResult> searchFaces(String collectionId, String bucketName) {
 		List<FaceMatchResult> faceMatchResultList = new ArrayList<FaceMatchResult>();
-		AmazonRekognition rekognitionClient = AmazonRekognitionClientBuilder.defaultClient();
-		final AmazonS3 s3 = AmazonS3ClientBuilder.standard().withRegion(Regions.US_EAST_1).build();
-		ListObjectsV2Result result = s3.listObjectsV2(bucketName);
-		List<S3ObjectSummary> s3objectsummaryList = result.getObjectSummaries();
-		cleanExistingCollection(collectionId);
-		createCollection(collectionId);
-		addFacesToCollection(collectionId,bucketName);
-		s3objectsummaryList.forEach(x -> {			
-			deleteFaceFromCollection(collectionId,x.getKey());
-			FaceMatchResult faceMatchResult = searchFaces(collectionId, bucketName, x.getKey());
+		s3objectsummaryList.forEach(x -> {
+			FaceMatchResult faceMatchResult = new FaceMatchResult();
+			List<Score> scoreList = new ArrayList<Score>();
+			faceMatchResult.setReference_face(x.getKey());	
+			
+			s3objectsummaryList.forEach(y -> {
+				Score score = new Score();
+				score.setMatched_face(y.getKey());		
+				Float similarity = compareTest(bucketName, x.getKey(), y.getKey());
+				score.setScore(similarity);
+				scoreList.add(score);
+			});
+			faceMatchResult.setScores(scoreList);
 			faceMatchResultList.add(faceMatchResult);
-			indexFaces(collectionId, bucketName, x.getKey());
 		});
 		return faceMatchResultList;
 	}
-	FaceMatchResult searchFaces(String collectionId, String bucketName, String object) {
-		FaceMatchResult result = new FaceMatchResult();
-		AmazonRekognition rekognitionClient = AmazonRekognitionClientBuilder.defaultClient();
-		ObjectMapper objectMapper = new ObjectMapper();
-
-		// Get an image object from S3 bucket.
-		Image image=new Image()
-				.withS3Object(new S3Object()
-						.withBucket(bucketName)
-						.withName(object));
-
-		// Search collection for faces similar to the largest face in the image.
-		SearchFacesByImageRequest searchFacesByImageRequest = new SearchFacesByImageRequest()
-				.withCollectionId(collectionId)
-				.withImage(image)
-				.withFaceMatchThreshold(70F)
-				.withMaxFaces(5);
-
-		SearchFacesByImageResult searchFacesByImageResult = 
-				rekognitionClient.searchFacesByImage(searchFacesByImageRequest);
-
-		System.out.println("Faces matching largest face in image from" + object);
-		List < FaceMatch > faceImageMatches = searchFacesByImageResult.getFaceMatches();
-		result.setReference_face(object);
-		List<Score> scoreList = new ArrayList<Score>();
-		for (FaceMatch face: faceImageMatches) {
-			
-			Score score = new Score();
-			score.setScore(face.getSimilarity());
-			score.setMatched_face(face.getFace().getExternalImageId());
-			
-			/*try {
-				System.out.println(objectMapper.writerWithDefaultPrettyPrinter()
-						.writeValueAsString(face));
-			} catch (JsonProcessingException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			System.out.println();*/
-			scoreList.add(score);
+	
+	public Float compareTest(String bucketName, String sourceImage, String targetImage) {
+		AmazonRekognition client = AmazonRekognitionClientBuilder.standard().build();
+		CompareFacesRequest request = new CompareFacesRequest()
+		        .withSourceImage(new Image().withS3Object(new S3Object().withBucket(bucketName).withName(sourceImage)))
+		        .withTargetImage(new Image().withS3Object(new S3Object().withBucket(bucketName).withName(targetImage))).withSimilarityThreshold(0f);
+		CompareFacesResult response = client.compareFaces(request);
+		ComparedSourceImageFace y = response.getSourceImageFace();
+		List<CompareFacesMatch> matchList = response.getFaceMatches();
+		Float similarity = 0f;
+		if(!matchList.isEmpty() && matchList.size()>0) {
+		CompareFacesMatch faceMatch = matchList.get(0);
+		similarity = faceMatch.getSimilarity();
 		}
-		result.setScores(scoreList);
-		return result;
-	}
-
-	void deleteFaceFromCollection(String collectionId, String object) {
-		AmazonRekognition rekognitionClient = AmazonRekognitionClientBuilder.defaultClient();
-
-
-		DeleteFacesRequest deleteFacesRequest = new DeleteFacesRequest()
-				.withCollectionId(collectionId)
-				.withFaceIds(listFaces(collectionId, object));
-
-
-		DeleteFacesResult deleteFacesResult=rekognitionClient.deleteFaces(deleteFacesRequest);
-
+		return similarity;
+		
 		/*
-		 * List < String > faceRecords = deleteFacesResult.getDeletedFaces();
-		 * System.out.println(Integer.toString(faceRecords.size()) +
-		 * " face(s) deleted:"); for (String face: faceRecords) {
-		 * System.out.println("FaceID: " + face); }
+		 * ComparedFace x = faceMatch.getFace(); x.getConfidence();
 		 */
-
 	}
-
-	String listFaces(String collectionId, String object) {
-		AmazonRekognition rekognitionClient = AmazonRekognitionClientBuilder.defaultClient();
-
-		ObjectMapper objectMapper = new ObjectMapper();
-
-		ListFacesResult listFacesResult = null;
-		//System.out.println("Faces in collection " + collectionId);
-
-		String paginationToken = null;
-		do {
-			if (listFacesResult != null) {
-				paginationToken = listFacesResult.getNextToken();
-			}
-
-			ListFacesRequest listFacesRequest = new ListFacesRequest()
-					.withCollectionId(collectionId)
-					.withMaxResults(1)
-					.withNextToken(paginationToken);
-
-			listFacesResult =  rekognitionClient.listFaces(listFacesRequest);
-			List < Face > faces = listFacesResult.getFaces();
-			for (Face face: faces) {
-				if(face.getExternalImageId().equalsIgnoreCase(object)) {
-					return face.getFaceId();
-				}
-				/*try {
-					System.out.println(objectMapper.writerWithDefaultPrettyPrinter()
-							.writeValueAsString(face));
-					if(face.getExternalImageId().equalsIgnoreCase(object)) {
-						return face.getFaceId();
-					}
-				} catch (JsonProcessingException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}*/
-			}
-		} while (listFacesResult != null && listFacesResult.getNextToken() !=
-				null);
-		return null;
-	}
+	
 
 
 
